82974fc93aec54ea511eb9d87c440543ac7158cd
Rachid bouhouch <rbouhouch@sqli.com>
2023-05-10 09:47:02 +0100

    add Customer Projection + Poduct Projection

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,48 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repo.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository){
+		return args -> {
+			productRepository.saveAll(
+					List.of(
+							Product.builder()
+									.name("Iphone 6")
+									.price(1500.0)
+									.quantity(15)
+									.build(),
+							Product.builder()
+									.name("Samsung J1")
+									.price(500.0)
+									.quantity(2)
+									.build(),
+							Product.builder()
+									.name("Mac")
+									.price(10000.0)
+									.quantity(5)
+									.build()
+					)
+			);
+			productRepository.findAll().forEach(
+					System.out::println
+			);
+		};
 	}
 
 }
